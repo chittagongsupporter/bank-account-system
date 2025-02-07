2bcf17054db394aaff97d6ec428a57189d3054ae
Srivatsan <talkads123@gmail.com>
2025-02-07 04:50:21 +0000

    remove class in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,3 @@
 #include <iostream>
 
-// define a class named 'BankAccount'
-class BankAccount {
-public:
-  // data members
-  int accountBalance;
-  int accountNumber;
-
-  // member functions
-  void deposit(int amount) { accountBalance += amount; }
-  void withdraw(int amount) { accountBalance -= amount; }
-};
-
-int main() {
-
-  return 0;
-}
+int main() { return 0; }
